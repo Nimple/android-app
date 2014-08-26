150c09f1ee810e3077a878f8fee0fb86e6ff1e59
Ben John <john@stocard.de>
2014-08-26 23:25:35 +0200

    New config and new code-style (tabs instead of spaces).

--- nimpleandroid/src/main/java/de/nimple/config/Config.java
+++ nimpleandroid/src/main/java/de/nimple/config/Config.java
@@ -1,13 +1,17 @@
 package de.nimple.config;
 
-import android.os.Build;
-
 import de.nimple.BuildConfig;
 
-public abstract class Config {
-    // dev token
-    public static final String MIXPANEL_TOKEN = "6e3eeca24e9b2372e8582b381295ca0c";
+final public class Config {
+	public static final String MIXPANEL_TOKEN;
 
-    // prod token
-    //public static final String MIXPANEL_TOKEN = "c0d8c866df9c197644c6087495151c7e";
+	static {
+		if (BuildConfig.DEBUG) {
+			// dev token
+			MIXPANEL_TOKEN = "6e3eeca24e9b2372e8582b381295ca0c";
+		} else {
+			// prod token
+			MIXPANEL_TOKEN = "c0d8c866df9c197644c6087495151c7e";
+		}
+	}
 }
