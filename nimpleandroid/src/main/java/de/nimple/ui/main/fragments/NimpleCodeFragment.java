1c635954626214743d40f224b94985843f126f08
dennis <dennis@appstronauten.com>
2014-09-23 14:59:48 +0200

    finalized dropdown for cards

--- nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCodeFragment.java
+++ nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCodeFragment.java
@@ -19,6 +19,8 @@ import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Spinner;
 
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
