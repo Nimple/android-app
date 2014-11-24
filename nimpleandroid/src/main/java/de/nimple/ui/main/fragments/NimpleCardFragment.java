510af94d7ec119fdc91e685e47ed30c74e50b153
apstroDennis <dennis@appstronauten.com>
2014-11-24 19:46:18 +0100

    solved #62

--- nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCardFragment.java
+++ nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCardFragment.java
@@ -182,7 +182,11 @@ public class NimpleCardFragment extends Fragment implements IExportExtender {
     public void delCard(){
         final NimpleCodeHelper ncode = new NimpleCodeHelper(ctx);
         if(ncode.holder.id != 0) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            ncode.delete(ncode.holder);
+            ncode.setCurrentId(0);
+            refreshUi();
+            EventBus.getDefault().post(new NimpleCodeChangedEvent());
+           /* AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(getString(R.string.del_ncard_question));
             builder.setCancelable(true);
             builder.setPositiveButton(getString(R.string.button_ok), new DialogInterface.OnClickListener() {
@@ -202,7 +206,7 @@ public class NimpleCardFragment extends Fragment implements IExportExtender {
                 }
             });
             AlertDialog dialog = builder.create();
-            dialog.show();
+            dialog.show();*/
         }else{
             Toast.makeText(ctx, "Die letzte Nimple Karte kann nicht gelöscht werden", Toast.LENGTH_SHORT).show();
         }
--- nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCodeFragment.java
+++ nimpleandroid/src/main/java/de/nimple/ui/main/fragments/NimpleCodeFragment.java
@@ -175,7 +175,11 @@ public class NimpleCodeFragment extends Fragment implements IExportExtender {
     public void delCard(){
         final NimpleCodeHelper ncode = new NimpleCodeHelper(ctx);
         if(ncode.holder.id != 0) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            ncode.delete(ncode.holder);
+            ncode.setCurrentId(0);
+            refreshUi();
+            EventBus.getDefault().post(new NimpleCodeChangedEvent());
+            /*AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(getString(R.string.del_ncard_question));
             builder.setCancelable(true);
             builder.setPositiveButton(getString(R.string.button_ok), new DialogInterface.OnClickListener() {
@@ -195,7 +199,7 @@ public class NimpleCodeFragment extends Fragment implements IExportExtender {
                 }
             });
             AlertDialog dialog = builder.create();
-            dialog.show();
+            dialog.show();*/
         }else{
             Toast.makeText(ctx, "Die letzte Nimple Karte kann nicht gelöscht werden", Toast.LENGTH_SHORT).show();
         }
